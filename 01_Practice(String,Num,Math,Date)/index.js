19df881f34df7ac59845f8db55c0ef09646c211f
Jahanvi215 <tharu.jahanvi215@gmail.com>
2024-12-11 00:07:18 +0530

    JS revision

---
+++ 01_Practice(String,Num,Math,Date)/index.js
@@ -0,0 +1,108 @@
+// string manipulation
+// console.log("jahanvi");
+//  const string1 = new String("Johnson")
+//  console.log(string1);
+//  console.log(string1.charAt(5));
+//  console.log(string1.indexOf('h'));
+
+/*********Number **********/
+
+/**const num1 = new Number(400)
+console.log(num1);
+console.log(num1.toString());
+console.log(num1.toFixed(2));
+
+const num2 = new Number(345.889)
+console.log(num2.toPrecision(5));
+const num3 = 10000000000
+console.log(num3.toLocaleString('en-IN'));*/
+
+/*************Maths************** */
+
+// console.log(Math);
+// console.log(Math.abs(-9));
+// console.log(Math.round(5.88));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.98));
+// console.log(Math.sqrt(25555));
+
+// console.log(Math.min(4,6,2,7));
+// console.log(Math.max(9,44,3));
+
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor((Math.random()*10)+1));
+
+// const min = 10
+// const max =20
+
+// const value = (Math.floor(Math.random()*(max - min + 1))) + min
+// console.log("******", value);
+
+/***********************************DATE & TIME************************************************** */
+
+const myDate = new Date()
+
+// console.log(myDate);
+// console.log(myDate.toString(),"----toString");
+// console.log(myDate.toDateString(),"----toDateString");
+// console.log(myDate.toISOString(),"----toIOSString");
+// console.log(myDate.toJSON(),"----toJSON");
+// console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toUTCString());
+
+// const myCreatedDate = new Date(2019, 2, 14, 6, 9)
+// const myCreatedDate = new Date("1-20-2000")
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleString());
+
+const date2 = new Date()
+console.log(date2.getDate());
+console.log(date2.getMonth()+ 1);
+console.log(date2.getFullYear());
+console.log(date2.getTime());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+ 
+ 
+ 
